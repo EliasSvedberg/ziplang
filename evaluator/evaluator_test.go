5a9d09f9e128b9ee8051ee42d721c29e2b3f2482
Elias Svedberg <eliassvedberg@pop-os.localdomain>
2024-11-04 22:16:30 +0100

    Evaluator: Add tests to the evaluator

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -7,6 +7,35 @@ import (
   "ziplang/object"
 )
 
+func TestEvalatorStringExpression(t *testing.T) {
+  tests := []struct {
+    input string
+    expectedOutput interface{}
+  }{
+    {`"teststring"`, `"teststring"`},
+  }
+
+  for _, tc := range tests {
+    l := lexer.New(tc.input)
+    p := parser.New(l)
+
+    program := p.Parse()
+    env := object.NewEnvironment()
+
+    evaluatedProgram := Evaluate(program, env)
+
+    result, ok := evaluatedProgram.(*object.String)
+
+    if !ok {
+      t.Errorf("object.Object is not a String. got=%T (%v)", evaluatedProgram, evaluatedProgram)
+    }
+
+    if result.Value != tc.expectedOutput {
+      t.Errorf("object has wrong value. got=%s, want=%s", result.Value, tc.expectedOutput)
+    }
+  }
+}
+
 func TestEvaluatorNumberExpression(t *testing.T) {
   tests := []struct {
     input string
